f163b4db5ec21b3032c6fed64589e7e997152aa4
Pritam Chakraborty <pritamchakrabortyshuvo@gmail.com>
2025-02-25 11:07:36 +0600

    Delete Solutions-Architect-Associate/User_Data_Nginx01.sh

--- Solutions-Architect-Associate/User_Data_Nginx01.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update -y  
-apt install -y nginx  
-systemctl enable --now nginx  
-# Create a basic HTML page
-echo "<h1>Welcome to Nginx on EC2</h1>" > /var/www/html/index.html  
-# Restart Nginx to apply changes
-systemctl restart nginx  
